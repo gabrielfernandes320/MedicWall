9e8ba3cc91afe5e72b444c64653fe1a33a71b4b0
Bruno <brunofirme.crema@hotmail.com>
2019-09-13 21:28:00 -0300

    something

---
+++ MedicWall/Model/Client.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace MedicWall.Model
+{
+    [Table("client")]
+    public class Client
+    {
+        [Key]
+        public int id { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+        public int password { get; set; }
+        public int role { get; set; }
+        public int cellPhone { get; set; }
+    }
+}
